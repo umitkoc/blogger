d8542e26f9f4bee642ea594b867a5bffc063ffb7
umitkoc <umitkoc.com@gmail.com>
2021-09-28 01:11:32 +0300

    update database and migrations InitialCreate2

---
+++ data/Migrations/20210927220830_InitialCreate2.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace data.Migrations
+{
+    public partial class InitialCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BogId",
+                table: "Tags");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BogId",
+                table: "Tags",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
